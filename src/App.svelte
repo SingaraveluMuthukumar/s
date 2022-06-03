892ef46fa283a1146b3d7304a1d5c5610dbe121d
SingaraveluMuthukumar <90843085+SingaraveluMuthukumar@users.noreply.github.com>
2022-06-03 13:25:39 +0530

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import Greeter from './greeter.svelte';
+
+  let counter = 0;
+
+  function handleClick() {
+    counter++;
+  }
+</script>
+
+<Greeter who="world" />
+
+<button
+  on:click={handleClick}
+  class="bg-red rounded border border-black p-2 m-10"
+  >Button clicked {counter} times</button
+>
+
+<style>
+</style>
